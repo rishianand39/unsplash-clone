d2ab5f60d7e58f9ccf5995d4078301c3bc8619ba
Rishi Anand <rishi.rn818@gmail.com>
2022-07-24 19:24:49 +0530

    form submit action added

--- frontend/src/Redux/auth/action.js
+++ frontend/src/Redux/auth/action.js
@@ -1,82 +1,93 @@
-
-
 import axios from "axios";
-
 export const actionTypes = {
-    AUTH_LOADING: "AUTH_LOADING",
-    AUTH_SUCCESS: "AUTH_SUCCESS",
-    AUTH_FAILURE: "AUTH_FAILURE",
+  AUTH_LOADING: "AUTH_LOADING",
+  AUTH_SUCCESS: "AUTH_SUCCESS",
+  AUTH_FAILURE: "AUTH_FAILURE",
+  AUTH_STATUS:"AUTH_STATUS"
+};
+
+// REGISTER ACTION
+export const register_loading = () => {
+  return {
+    type: actionTypes.AUTH_LOADING,
   };
-  
-  
-  // REGISTER ACTION
-  export const register_loading = () => {
-    return {
-      type: actionTypes.AUTH_LOADING,
-    };
+};
+export const register_status = () => {
+  return {
+    type: actionTypes.AUTH_STATUS,
   };
-  export const register_success = (data) => {
-    return {
-      type: actionTypes.AUTH_SUCCESS,
-    };
+};
+export const register_failure = () => {
+  return {
+    type: actionTypes.AUTH_FAILURE,
   };
-  export const register_failure = () => {
-    return {
-      type: actionTypes.AUTH_FAILURE,
-    };
+};
+
+
+
+// LOGIN ACTION
+export const login_loading = () => {
+  return {
+    type: actionTypes.AUTH_LOADING,
   };
-  
-  // LOGIN ACTION
-  export const login_loading = () => {
-    return {
-      type: actionTypes.AUTH_LOADING,
-    };
+};
+export const login_success = (data) => {
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    payload: data,
   };
-  export const login_success = (data) => {
-    return {
-      type: actionTypes.AUTH_SUCCESS,
-      payload:data
-    };
+};
+export const login_failure = () => {
+  return {
+    type: actionTypes.AUTH_FAILURE,
   };
-  export const login_failure = () => {
-    return {
-      type: actionTypes.AUTH_FAILURE,
-    };
-  };
-  
-  
-  // REGISTER
-  export const register =({ first_name, last_name, email, username, password }) =>
-    async (dispatch) => {
-      dispatch(register_loading());
-      try {
-         await axios.post("https://unsplash-clone-application.herokuapp.com/api/auth/register", {
+};
+
+
+// REGISTER
+
+export const register =
+  ({ first_name, last_name, email, username, password }) =>
+  async (dispatch) => {
+    dispatch(register_loading());
+    try {
+      await axios.post(
+        "https://unsplash-clone-application.herokuapp.com/api/auth/register",
+        {
           first_name: first_name,
           last_name: last_name,
           username: username,
           email: email,
           password: password,
-        });
-        // return dispatch(register_success())
-      } catch (error) {
-        return dispatch(register_failure());
-      }
-    };
-  
-  //   LOGIN
-  export const login =
-    ({ email, password }) =>
-    async (dispatch) => {
-      dispatch(login_loading());
-      try {
-        const res = await axios.post("https://unsplash-clone-application.herokuapp.com/api/auth/login", {
+        }
+      );
+       dispatch(register_status())
+    } catch (error) {
+       dispatch(register_failure());
+    }
+  };
+
+
+
+
+
+
+
+//   LOGIN
+export const login =({ email, password }) =>async (dispatch) => {
+
+  dispatch(login_loading())
+    try {
+      const res = await axios.post(
+        "https://unsplash-clone-application.herokuapp.com/api/auth/login",
+        {
           email: email,
           password: password,
-        });
-        console.log(res)
-        return dispatch(login_success(res.data));
-      } catch (error) {
-        return dispatch(login_failure());
-      }
-    };
-  
+        }
+      );
+      console.log(res.data)
+       dispatch(login_success(res.data));
+    } catch (error) {
+       dispatch(login_failure());
+    }
+  };
